f2c75e24aea65c18b866e46cc0d14c19eca91950
eunapio <roberto.eunapio@hotmail.com>
2025-03-19 16:18:08 -0300

    feat: ensure transaction table labels are rendered properly

--- prisma/migrations/20250319133838_migracao_extra/migration.sql
+++ prisma/migrations/20250319133838_migracao_extra/migration.sql
@@ -1,10 +1,10 @@
 /*
   Warnings:
 
-  - You are about to drop the column `catagory` on the `Transaction` table. All the data in the column will be lost.
+  - You are about to drop the column `category` on the `Transaction` table. All the data in the column will be lost.
   - Added the required column `category` to the `Transaction` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
-ALTER TABLE "Transaction" DROP COLUMN "catagory",
+ALTER TABLE "Transaction" DROP COLUMN "category",
 ADD COLUMN     "category" "TransactionCategory" NOT NULL;
